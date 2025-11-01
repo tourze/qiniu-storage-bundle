14f49965b2481338392cdaf791f3b3c3fe7b4638
github-ci <you@example.com>
2025-11-02 03:22:32 +0800

    update++++++

--- tests/Request/GetBucketsRequestTest.php
+++ tests/Request/GetBucketsRequestTest.php
@@ -2,7 +2,7 @@
 
 namespace QiniuStorageBundle\Tests\Request;
 
-use HttpClientBundle\Tests\Request\RequestTestCase;
+use HttpClientBundle\Test\RequestTestCase;
 use PHPUnit\Framework\Attributes\CoversClass;
 use QiniuStorageBundle\Request\GetBucketsRequest;
 
